a31d8b94fb383a346682e12c34401155d49037e3
Bart <dje21dje@gmail.com>
2021-05-23 22:41:55 +0200

    Add project files.

---
+++ SortowanieKart/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortowanieKart
+{
+    class Card
+    {
+        public Suits Suit { get; set; }
+        public Values Value { get; set; }
+        public string Name
+        {
+            get { return Value.ToString() + " of " + Suit.ToString(); }
+        }
+        public Card(Suits suit, Values value)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+    }
+}
---
+++ SortowanieKart/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortowanieKart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            Console.WriteLine("Pięć losowych kart:\n");
+
+            List<Card> cards = new List<Card>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                cards.Add(new Card((Suits)rand.Next(4), (Values)rand.Next(1, 14)));
+                Console.WriteLine(cards[i].Name);
+            }
+            
+            Console.WriteLine("\nKarty posortowane:\n");
+            //CardComparer_byValue comparer_ByValue = new CardComparer_byValue();
+            cards.Sort(new CardComparer_byValue());
+            foreach (Card card in cards)
+            {
+                Console.WriteLine(card.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
